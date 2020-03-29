b27bcdc3595bef387b12d2f0578341234af4e05e
Alex Wang <kaw293@cornell.edu>
2020-03-29 18:13:17 -0400

    Run black on everything

--- biases/datasets.py
+++ biases/datasets.py
@@ -1,4 +1,3 @@
-
 import math
 import torch
 import torchvision.transforms as transforms
@@ -12,39 +11,56 @@ from torch.utils.data import Dataset
 from oil.utils.utils import Named, export, Expression, FixedNumpySeed
 from biases.hamiltonian import ChainPendulum
 
+
 @export
-class RigidBodyDataset(Dataset,metaclass=Named):
-    space_dim=2
+class RigidBodyDataset(Dataset, metaclass=Named):
+    space_dim = 2
     num_targets = 1
-    def __init__(self, root_dir=None,body=ChainPendulum(3), n_systems=100, regen=False,
-                 chunk_len=5,dt=.1,integration_time=50):
+
+    def __init__(
+        self,
+        root_dir=None,
+        body=ChainPendulum(3),
+        n_systems=100,
+        regen=False,
+        chunk_len=5,
+        dt=0.1,
+        integration_time=50,
+    ):
         super().__init__()
-        root_dir = root_dir or os.path.expanduser(f'~/datasets/ODEDynamics/{self.__class__}/')
+        root_dir = root_dir or os.path.expanduser(
+            f"~/datasets/ODEDynamics/{self.__class__}/"
+        )
         self.body = body
-        filename = os.path.join(root_dir, f"trajectories_N{n_systems}_dt{dt}_T{integration_time}.pz")
+        filename = os.path.join(
+            root_dir, f"trajectories_N{n_systems}_dt{dt}_T{integration_time}.pz"
+        )
         if os.path.exists(filename) and not regen:
             ts, zs = torch.load(filename)
         else:
-            ts, zs = self.generate_trajectory_data(n_systems,dt,integration_time)
+            ts, zs = self.generate_trajectory_data(n_systems, dt, integration_time)
             os.makedirs(root_dir, exist_ok=True)
-            torch.save((ts, zs),filename)
+            torch.save((ts, zs), filename)
         self.Ts, self.Zs = self.chunk_training_data(ts, zs, chunk_len)
+
     def __len__(self):
         return self.Zs.shape[0]
 
     def __getitem__(self, i):
         return (self.Zs[i, 0], self.Ts[i]), self.Zs[i]
 
-    def generate_trajectory_data(self, n_systems, dt,integration_time, bs=100):
+    def generate_trajectory_data(self, n_systems, dt, integration_time, bs=100):
         """ Returns ts: (n_systems, traj_len) zs: (n_systems, traj_len, z_dim) """
         bs = min(bs, n_systems)
         n_gen = 0
         t_batches, z_batches, sysp_batches = [], [], []
         while n_gen < n_systems:
             z0s = self.sample_system(bs)
-            ts = torch.arange(0,integration_time,dt,device=z0s.device,dtype=z0s.dtype)
-            new_zs = self.body.integrate(z0s,ts)
-            t_batches.append(ts[None].repeat(bs,1))
+            ts = torch.arange(
+                0, integration_time, dt, device=z0s.device, dtype=z0s.dtype
+            )
+            new_zs = self.body.integrate(z0s, ts)
+            t_batches.append(ts[None].repeat(bs, 1))
             z_batches.append(new_zs)
             n_gen += bs
             print(n_gen)
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 import sys, os
 
-setup(name="hamiltonian-biases",
-      description="",
-      version='0.1',
-      author='',
-      author_email='maf820@nyu.edu',
-      license='MIT',
-      python_requires='>=3.6',
-      #install_requires=['networkx','LieConv @ git+https://github.com/mfinzi/LieConv'],#
-      #extras_require = {
-      #    'TBX':['tensorboardX']
-      #},
-      packages=find_packages(),
-      long_description=open('README.md').read(),
+setup(
+    name="hamiltonian-biases",
+    description="",
+    version="0.1",
+    author="",
+    author_email="maf820@nyu.edu",
+    license="MIT",
+    python_requires=">=3.6",
+    # install_requires=['networkx','LieConv @ git+https://github.com/mfinzi/LieConv'],#
+    # extras_require = {
+    #    'TBX':['tensorboardX']
+    # },
+    packages=find_packages(),
+    long_description=open("README.md").read(),
 )
