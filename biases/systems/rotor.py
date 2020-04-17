ade45305e598175318db4daacb50f8d603a01007
Alex Wang <kaw293@cornell.edu>
2020-04-17 16:16:54 -0400

    Fix Rotor syntax error

--- biases/systems/rotor.py
+++ biases/systems/rotor.py
@@ -1,17 +1,16 @@
-import torch
-import networkx as nx
 from oil.utils.utils import export
-from biases.systems.rigid_body import RigidBody,BodyGraph
-from biases.animation import Animation
-import numpy as np
+from biases.systems.rigid_body import RigidBody, BodyGraph
+
 
 @export
 class Rotor(RigidBody):
-    def __init__(self, mass=1,moments=(1,2,3)):
+    def __init__(self, mass=1, moments=(1, 2, 3)):
         self.body_graph = BodyGraph()
-        self.body_graph.add_extended_nd(0,m=mass,moments,d=3):
-    def sample_initial_conditions(self,N):
+        self.body_graph.add_extended_nd(key=0, m=mass, moments=moments, d=3)
+
+    def sample_initial_conditions(self, N):
         raise NotImplementedError
-        #i = self.body.key2id[node]
-    def potential(self,x):
+        # i = self.body.key2id[node]
+
+    def potential(self, x):
         return 0
